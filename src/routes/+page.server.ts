94ea377bc1034a5c17ea12b19e88f2f334605323
Darsh Dudhia <69112471+darshdudhia@users.noreply.github.com>
2026-09-17 11:05:47 +0530

    Reduce cache duration to 60 seconds

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 export const load = ({ setHeaders }) => {
 	setHeaders({
-		'cache-control': 'max-age=14400, must-revalidate, private'
+		'cache-control': 'max-age=60, must-revalidate, private'
 	});
 };
 export const ssr = false; // prevent flash of content from SSR before animations
